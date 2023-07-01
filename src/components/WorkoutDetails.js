1b322f6235b4f932e774aecd1f92578e491aea03
Benjikoy <francs.abundo@gmail.com>
2023-07-01 22:54:28 +0800

    done

--- src/components/WorkoutDetails.js
+++ src/components/WorkoutDetails.js
@@ -1,40 +1,58 @@
-import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
-import { useAuthContext } from '../hooks/useAuthContext'
-
-// date fns
-import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { useAuthContext } from "../hooks/useAuthContext";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
 const WorkoutDetails = ({ workout }) => {
-  const { dispatch } = useWorkoutsContext()
-  const { user } = useAuthContext()
+  const { dispatch } = useWorkoutsContext();
+  const { user } = useAuthContext();
 
   const handleClick = async () => {
     if (!user) {
-      return
+      return;
     }
 
-    const response = await fetch('/api/workouts/' + workout._id, {
-      method: 'DELETE',
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
       headers: {
-        'Authorization': `Bearer ${user.token}`
-      }
-    })
-    const json = await response.json()
+        Authorization: `Bearer ${user.token}`,
+      },
+    });
+    const json = await response.json();
 
     if (response.ok) {
-      dispatch({type: 'DELETE_WORKOUT', payload: json})
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
     }
+  };
+
+  let workoutDetailsClassName = "workout-details"; // Default className
+
+  if (workout.workoutType === "B") {
+    workoutDetailsClassName = "workout-details-b"; // Change className for workoutType B
+  } else if (workout.workoutType === "C") {
+    workoutDetailsClassName = "workout-details-c"; // Change className for workoutType C
   }
 
   return (
-    <div className="workout-details">
+    <div className={workoutDetailsClassName}>
       <h4>{workout.title}</h4>
-      <p><strong>Load (kg): </strong>{workout.load}</p>
-      <p><strong>Reps: </strong>{workout.reps}</p>
+      <p>
+        <strong>Load (kg):</strong> {workout.load}
+      </p>
+      <p>
+        <strong>Reps:</strong> {workout.reps}
+      </p>
+      <p>
+        <strong>Sets:</strong> {workout.sets}
+      </p>
+      <p>
+        <strong>Workout Type:</strong> {workout.workoutType}
+      </p>
       <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
-      <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
+      <span className="material-symbols-outlined" onClick={handleClick}>
+        delete
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default WorkoutDetails
+export default WorkoutDetails;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,76 @@
-import { useEffect }from 'react'
-import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
-import { useAuthContext } from "../hooks/useAuthContext"
+import { useState, useEffect } from 'react';
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 // components
-import WorkoutDetails from '../components/WorkoutDetails'
-import WorkoutForm from '../components/WorkoutForm'
+import WorkoutDetails from '../components/WorkoutDetails';
+import WorkoutForm from '../components/WorkoutForm';
 
 const Home = () => {
-  const {workouts, dispatch} = useWorkoutsContext()
-  const {user} = useAuthContext()
+  const { workouts, dispatch } = useWorkoutsContext();
+  const { user } = useAuthContext();
+  const [activeTab, setActiveTab] = useState('all');
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('/api/workouts', {
-        headers: {'Authorization': `Bearer ${user.token}`},
-      })
-      const json = await response.json()
+      const url = activeTab === 'all' ? '/api/workouts' : `/api/workouts?type=${activeTab}`;
+      const response = await fetch(url, {
+        headers: { Authorization: `Bearer ${user.token}` },
+      });
+      const json = await response.json();
 
       if (response.ok) {
-        dispatch({type: 'SET_WORKOUTS', payload: json})
+        dispatch({ type: 'SET_WORKOUTS', payload: json });
       }
-    }
+    };
 
     if (user) {
-      fetchWorkouts()
+      fetchWorkouts();
     }
-  }, [dispatch, user])
+  }, [activeTab, dispatch, user]);
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
 
   return (
-    <div className="home">
-      <div className="workouts">
-        {workouts && workouts.map((workout) => (
-          <WorkoutDetails key={workout._id} workout={workout} />
-        ))}
+    <div>
+      <div className='filterSection'>
+        <button
+          className={activeTab === 'all' ? 'active' : ''}
+          onClick={() => handleTabClick('all')}
+        >
+          All workouts
+        </button>
+        <button
+          className={activeTab === 'A' ? 'active' : ''}
+          onClick={() => handleTabClick('A')}
+        >
+          Day A
+        </button>
+        <button
+          className={activeTab === 'B' ? 'active' : ''}
+          onClick={() => handleTabClick('B')}
+        >
+          Day B
+        </button>
+        <button
+          className={activeTab === 'C' ? 'active' : ''}
+          onClick={() => handleTabClick('C')}
+        >
+          Day C
+        </button>
       </div>
-      <WorkoutForm />
+      <div className="home">
+        <div className="workouts">
+          {workouts && workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+        </div>
+        <WorkoutForm />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
